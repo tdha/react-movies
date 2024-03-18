fa224f344a273c6134ac0eb09c1495c12bd86e46
Ton Ha <ton@tdha.co>
2024-03-18 15:34:26 +1000

    CSS updates

--- src/pages/MovieDetailPage/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage/MovieDetailPage.jsx
@@ -1,7 +1,18 @@
+import { useParams } from "react-router-dom";
+import { movies } from '../../data';
+
 function MovieDetailPage() {
-    return (
-        <h1>MovieDetailPage</h1>
-    );
-}
+    const { movieName } = useParams();
+    const movie = movies.find(movie => movie.title === movieName);
+
+    return movie ? (
+        <div className="movieCard">
+            <h2 className="movieTitle">{movie.title}</h2>
+            <p className="movieReleaseDate">{movie.releaseDate}</p>
+            <img src={movie.posterPath} alt={movie.title} className="moviePoster" />
+            <p className="movieCast">{movie.cast.join(',')}</p>
+        </div>
+    ) : <p>Movie not found.</p>;
+} 
 
 export default MovieDetailPage
--- src/pages/MoviesListPage/MoviesListPage.jsx
+++ src/pages/MoviesListPage/MoviesListPage.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
+import { Link } from "react-router-dom";
 
 function MoviesListPage({ movies }) {
     console.log(movies);
     return (
         <div className="container">
             {movies.map((movie, index) => (
-            <MovieCard
+                <Link to={`/movies/${encodeURIComponent(movie.title)}`} key={index} style={{ textDecoration: 'none' }}>
+                <MovieCard
                 key={index}
                 posterPath={movie.posterPath}
                 title={movie.title}
                 releaseDate={movie.releaseDate}
             />
+            </Link>
             ))}
         </div>
     );
